5a27ab8c2c26f31b519c2790ee15fbc8d75eb65a
zao <zao@8e65eab9-f7f9-11de-b461-080020c656bc>
2010-03-28 23:04:54 +0000

    Multichannel and downmix support for GDI.

--- GdiFallback.cc
+++ GdiFallback.cc
@@ -1,5 +1,6 @@
 #include "PchSeekbar.h"
 #include "GdiFallback.h"
+#include "Helpers.h"
 
 namespace wave
 {
@@ -82,7 +83,7 @@ namespace wave
 			release_objects();
 			create_objects();
 		}
-		if (s & (state_data | state_size | state_orientation | state_color))
+		if (s & (state_data | state_size | state_orientation | state_color | state_channel_order | state_downmix_display))
 			update_data();
 #if 0
 		if (s & state_shade_played)
@@ -154,55 +155,86 @@ namespace wave
 		service_ptr_t<waveform> w;
 		if (callback.get_waveform(w))
 		{
-			pfc::list_hybrid_t<float, 2048> avg_min, avg_max, avg_rms;
-			//TODO: multichannel
-			w->get_field("minimum", 0, avg_min);
-			w->get_field("maximum", 0, avg_max);
-			w->get_field("rms", 0, avg_rms);
-			wave_dc->SelectPen(*pen_foreground);
+			if (callback.get_downmix_display())
+				w = downmix_waveform(w);
 
-			color bg = callback.get_color(config::color_background);
-			color txt = callback.get_color(config::color_foreground);
-			D3DXVECTOR4 backgroundColor(bg.r, bg.g, bg.b, bg.a);
-			D3DXVECTOR4 textColor(txt.r, txt.g, txt.b, txt.a);
-			D3DXVECTOR2 tc;
+			pfc::list_t<channel_info> infos;
+			callback.get_channel_infos(infos);
 
-			float dx = 1.0f / (float)size.cx;
-			float dy = 1.0f / (float)size.cy;
-			for (size_t x = 0; x < (size_t)size.cx; ++x)
+			auto channel_numbers = expand_flags(w->get_channel_map());
+			pfc::list_t<int> channel_indices;
+			infos.enumerate([&channel_indices, channel_numbers](channel_info const& info)
 			{
-				tc.x = (float)x / (float)size.cx;
-				size_t ix = x * 2048ul / size.cx;
-				D3DXVECTOR4 sample(avg_min[ix], avg_max[ix], avg_rms[ix], 1);
-#if 1
-				for (size_t y = 0; y < (size_t)size.cy; ++y)
+				if (info.enabled)
 				{
-					D3DXVECTOR4 c;
-					tc.y = 1.0f - 2.0f * (float)y / (float)size.cy;
-					float below = tc.y - sample.x;
-					float above = tc.y - sample.y;
-					float factor = std::min(fabs(below), fabs(above));
-					bool outside = (below < 0 || above > 0);
-					bool inside_rms = fabs(tc.y) <= sample.z;
-
-					if (outside)
-						c = backgroundColor;
-					else
-						D3DXVec4Lerp(&c, &backgroundColor, &textColor, 7.0f * factor);
-					
-					saturate(&c);
-					color cc(c.x, c.y, c.z, c.w);
-
-					wave_dc->SetPixelV(vertical ? y : x, vertical ? x : y, color_to_xbgr(cc));
+					auto I = std::find(channel_numbers.begin(), channel_numbers.end(), info.channel);
+					decltype(I) first = channel_numbers.begin();
+					if (I != channel_numbers.end())
+					{
+						channel_indices.add_item(std::distance(first, I));
+					}
 				}
-#else
-				size_t ix = std::min(2047ul, x * 2048ul / size.cx);
-				CPoint p1(x, (int)(size.cy * (0.5 - avg_min[ix] * 0.5)));
-				CPoint p2(x, (int)(size.cy * (0.5 - avg_max[ix] * 0.5)));
-				wave_dc->MoveTo(orientate(p1));
-				wave_dc->LineTo(orientate(p2));
-#endif
-			}
+			});
+
+			int quad_index = 0;
+			auto index_count = channel_indices.get_count();
+			channel_indices.enumerate([&, index_count](int index)
+			{
+				pfc::list_hybrid_t<float, 2048> avg_min, avg_max, avg_rms;
+				w->get_field("minimum", index, avg_min);
+				w->get_field("maximum", index, avg_max);
+				w->get_field("rms", index, avg_rms);
+				wave_dc->SelectPen(*pen_foreground);
+
+				color bg = callback.get_color(config::color_background);
+				color txt = callback.get_color(config::color_foreground);
+				D3DXVECTOR4 backgroundColor(bg.r, bg.g, bg.b, bg.a);
+				D3DXVECTOR4 textColor(txt.r, txt.g, txt.b, txt.a);
+				D3DXVECTOR2 tc;
+
+				float squash = (float)quad_index / (float)index_count;
+				auto& outer_size = size;
+				CSize size(outer_size.cx, outer_size.cy / index_count);
+
+				float dx = 1.0f / (float)size.cx;
+				float dy = 1.0f / (float)size.cy;
+				for (size_t x = 0; x < (size_t)size.cx; ++x)
+				{
+					tc.x = (float)x / (float)size.cx;
+					size_t ix = x * 2048ul / size.cx;
+					D3DXVECTOR4 sample(avg_min[ix], avg_max[ix], avg_rms[ix], 1);
+		#if 1
+					for (size_t y = 0; y < (size_t)size.cy; ++y)
+					{
+						size_t out_y = y + (size_t)(outer_size.cy * squash);
+						D3DXVECTOR4 c;
+						tc.y = 1.0f - 2.0f * (float)y / (float)size.cy;
+						float below = tc.y - sample.x;
+						float above = tc.y - sample.y;
+						float factor = std::min(fabs(below), fabs(above));
+						bool outside = (below < 0 || above > 0);
+						bool inside_rms = fabs(tc.y) <= sample.z;
+
+						if (outside)
+							c = backgroundColor;
+						else
+							D3DXVec4Lerp(&c, &backgroundColor, &textColor, 7.0f * factor);
+						
+						saturate(&c);
+						color cc(c.x, c.y, c.z, c.w);
+
+						wave_dc->SetPixelV(vertical ? out_y : x, vertical ? x : out_y, color_to_xbgr(cc));
+					}
+		#else
+					size_t ix = std::min(2047ul, x * 2048ul / size.cx);
+					CPoint p1(x, (int)(size.cy * (0.5 - avg_min[ix] * 0.5)));
+					CPoint p2(x, (int)(size.cy * (0.5 - avg_max[ix] * 0.5)));
+					wave_dc->MoveTo(orientate(p1));
+					wave_dc->LineTo(orientate(p2));
+		#endif
+				}
+				++quad_index;
+			});
 		}
 	}
 
