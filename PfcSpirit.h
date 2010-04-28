4703842c11d75235ce15476341a9730a4e806cfc
zao <zao@8e65eab9-f7f9-11de-b461-080020c656bc>
2010-04-28 12:55:07 +0000

    Spirit container support good enough for reading into a list_t with Qi.

---
+++ PfcSpirit.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "../../pfc/list.h"
+#include <boost/spirit/home/support.hpp>
+#include <boost/spirit/home/support/container.hpp>
+
+namespace boost { namespace spirit { namespace traits
+{
+	template <typename T>
+	struct is_container<typename pfc::list_t<T>>
+		: mpl::bool_<true>
+	{};
+
+	template <typename T>
+	struct container_value<typename pfc::list_t<T>>
+		: detail::remove_value_const<T>
+	{};
+
+	namespace detail
+	{
+		template <typename T>
+		struct pfc_list_const_iterator
+		{
+			boost::reference_wrapper<pfc::list_t<T> const> container;
+			t_size offset;
+		};
+
+		template <typename T>
+		struct pfc_list_iterator
+		{
+			boost::reference_wrapper<pfc::list_t<T>> container;
+			t_size offset;
+		};
+	}
+
+	template <typename T>
+	struct container_iterator<typename pfc::list_t<T> const>
+	{
+		typedef detail::pfc_list_iterator<typename container_value<T>::type> type;
+	};
+
+	template <typename T>
+	struct container_iterator<typename pfc::list_t<T>>
+	{
+		typedef detail::pfc_list_iterator<typename container_value<T>::type> type;
+	};
+
+	template <typename T>
+	bool push_back(typename pfc::list_t<T>& c, T const& val)
+	{
+		c.add_item(val);
+		return true;
+	}
+
+	template <typename T>
+	bool is_empty(typename pfc::list_t<T> const& c)
+	{
+		return !c.get_count();
+	}
+}}}
