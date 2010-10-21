6b5c7261888afc4560ced68894c7a8b90bb00bbb
zao <zao@8e65eab9-f7f9-11de-b461-080020c656bc>
2010-10-21 04:39:21 +0000

    Set thread name for worker threads.

--- Helpers.h
+++ Helpers.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <windows.h>
 
 template <typename F>
 auto try_module_call(F f) -> decltype(f())
@@ -13,6 +14,36 @@ auto try_module_call(F f) -> decltype(f())
 	{}
 	return ret;
 }
+
+const DWORD MS_VC_EXCEPTION=0x406D1388;
+
+inline void SetThreadName( DWORD dwThreadID, char const* threadName)
+{
+	#pragma pack(push,8)
+	typedef struct tagTHREADNAME_INFO
+	{
+	   DWORD dwType; // Must be 0x1000.
+	   LPCSTR szName; // Pointer to name (in user addr space).
+	   DWORD dwThreadID; // Thread ID (-1=caller thread).
+	   DWORD dwFlags; // Reserved for future use, must be zero.
+	} THREADNAME_INFO;
+	#pragma pack(pop)
+
+	THREADNAME_INFO info;
+	info.dwType = 0x1000;
+	info.szName = threadName;
+	info.dwThreadID = dwThreadID;
+	info.dwFlags = 0;
+
+	__try
+	{
+		RaiseException( MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info );
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
+}
+
 
 template <typename F>
 void in_main_thread(F f)
