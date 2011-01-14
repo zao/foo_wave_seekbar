bf8f7fbe15359d61a59406b9c7ecdc824f455d56
Lars Viklund <zao@zao.se>
2011-01-14 04:22:14 +0100

    Moved seeking tooltip out.

---
+++ SeekTooltip.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "SeekCallback.h"
+
+namespace wave
+{
+	struct seek_tooltip : seek_callback, noncopyable
+	{
+		explicit seek_tooltip(HWND parent)
+			: parent(parent)
+		{
+			tooltip.Create(nullptr);
+			toolinfo.cbSize = sizeof(toolinfo);
+			toolinfo.uFlags = TTF_TRACK | TTF_IDISHWND | TTF_ABSOLUTE | TTF_TRANSPARENT;
+			toolinfo.hwnd = parent;
+			toolinfo.uId = 0;
+			toolinfo.lpszText = L"";
+			toolinfo.hinst = core_api::get_my_instance();
+			tooltip.AddTool(&toolinfo);
+		}
+
+		~seek_tooltip()
+		{
+			tooltip.DelTool(&toolinfo);
+			tooltip.DestroyWindow();
+		}
+
+		virtual void on_seek_begin() override
+		{
+			show = true;
+			track_mouse();
+		}
+
+		virtual void on_seek_position(double time, bool legal) override
+		{
+			show = legal;
+			std::wstring txt = format_time(time);
+			toolinfo.lpszText = const_cast<wchar_t*>(txt.c_str());
+			tooltip.SetToolInfo(&toolinfo);
+			track_mouse();
+		}
+
+		virtual void on_seek_end(bool aborted) override
+		{
+			show = false;
+			tooltip.TrackActivate(&toolinfo, FALSE);
+		}
+		
+	private:
+		CToolTipCtrl tooltip;
+		TOOLINFO toolinfo;
+		CWindow parent;
+		bool show;
+		
+		void track_mouse()
+		{
+			POINT pos = {};
+			GetCursorPos(&pos);
+			tooltip.TrackPosition(pos.x + 10, pos.y - 20);
+			tooltip.TrackActivate(&toolinfo, show ? TRUE : FALSE);
+		}
+
+		std::wstring format_time(double time)
+		{
+			auto str = pfc::stringcvt::string_os_from_utf8(pfc::format_time(pfc::rint64(time)));
+			std::wstring out = str.get_ptr();
+			return out;
+		}
+	};
+}
