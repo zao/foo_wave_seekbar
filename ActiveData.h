201e91d946ae035c4565becbe7d562a70873a98a
Lars Viklund <zao@zao.se>
2011-01-15 18:59:48 +0100

    Removed string parameter.

--- ActiveData.h
+++ ActiveData.h
@@ -13,14 +13,14 @@ namespace wave
 	struct active_data_listener
 	{
 		virtual ~active_data_listener() {}
-		virtual void on_write(active_data<T> const& value, std::string const& source) {};
+		virtual void on_write(active_data<T> const& value) {};
 	};
 
 	template <typename T>
 	struct active_data_function_listener : active_data_listener<T>
 	{
-		typedef void (write_type)(active_data<T> const&, std::string const&);
-		virtual void on_write(active_data<T> const& value, std::string const& source) { if (write) write(value, source); }
+		typedef void (write_type)(active_data<T> const&);
+		virtual void on_write(active_data<T> const& value) { if (write) write(value); }
 		boost::function<write_type> write;
 	};
 
@@ -36,9 +36,8 @@ namespace wave
 	struct active_data
 	{
 		explicit active_data(T t = T()) : value(new T(t)), listeners(new listener_collection()) {}
-		T& operator = (T const& rhs) { return set(rhs, ""); }
-		T& set(T const& rhs, std::string const& who) { *value = rhs; on_write(who); return *value; }
-		operator T const & () const { return *value; }
+		T& set(T const& rhs) { *value = rhs; on_write(); return *value; }
+		T const& get() const { return *value; }
 
 		void subscribe(weak_ptr<active_data_listener<T>> ptr)
 		{
@@ -46,13 +45,13 @@ namespace wave
 		}
 
 	private:
-		void on_write(std::string const who)
+		void on_write()
 		{
 			for (auto I = listeners->begin(); I != listeners->end(); )
 			{	
 				if (auto ptr = I->lock())
 				{
-					ptr->on_write(*this, who);
+					ptr->on_write(*this);
 					++I;
 				}
 				else
@@ -65,7 +64,27 @@ namespace wave
 		typedef std::set<weak_ptr<active_data_listener<T>>> listener_collection;
 		boost::shared_ptr<T> value;
 		boost::shared_ptr<listener_collection> listeners;
+	};	
 
-		active_data<T>& operator = (active_data<T> const&);
+	struct get_impl
+	{
+		template <typename T>
+		struct result { typedef T const& type; };
+
+		template <typename T>
+		T const& operator () (active_data<T> const& ad) const { return ad.get(); }
 	};
+
+	static boost::phoenix::function<get_impl> get;
+
+	struct set_impl
+	{
+		template <typename T>
+		struct result { typedef T& type; };
+
+		template <typename T>
+		T& operator () (active_data<T>& ad, T const& t) const { return ad.set(t); }
+	};
+
+	static boost::phoenix::function<set_impl> set;
 };
