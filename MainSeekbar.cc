99dea240a11b88f9056c4592ac47a3bc1ad3037a
Lars Viklund <zao@zao.se>
2011-09-30 07:29:38 +0200

    Bumped version number to 0.2.13.13

--- MainSeekbar.cc
+++ MainSeekbar.cc
@@ -12,5 +12,5 @@ static service_factory_t<ui_element_impl<wave::seekbar_dui>> g_asdf;
 static uie::window_factory<wave::seekbar_uie_t<uie::type_panel>> g_sadf_panel;
 static uie::window_factory<wave::seekbar_uie_t<uie::type_toolbar>> g_sadf_toolbar;
 
-DECLARE_COMPONENT_VERSION("Waveform seekbar", "0.2.13.12", "Zao")
+DECLARE_COMPONENT_VERSION("Waveform seekbar", "0.2.13.13", "Zao")
 VALIDATE_COMPONENT_FILENAME("foo_wave_seekbar.dll")
