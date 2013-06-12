dfdf400a5fb380edfc3fc855e7306f4c9382fcf9
Lars Viklund <zao@zao.se>
2013-06-12 05:24:04 +0200

    Introduce performance trace functionality

---
+++ util/Profiling.h
@@ -0,0 +1,45 @@
+//          Copyright Lars Viklund 2008 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+namespace util
+{
+enum class Phase
+{
+	BEGIN_EVENT = 'B', END_EVENT = 'E',
+	INSTANT = 'I',
+	ASYNC_START = 'S', ASYNC_FINISH = 'F',
+};
+
+bool is_recording_enabled();
+void record_event(Phase phase, char const* category, char const* name, uint64_t const* id = nullptr);
+uint64_t generate_recording_id();
+
+template <Phase FromPhase, Phase ToPhase>
+struct EventScope
+{
+	EventScope(char const* category, char const* name)
+		: _category(category), _name(name)
+		, _id(generate_recording_id())
+	{
+		record_event(FromPhase, category, name, &_id);
+	}
+	~EventScope()
+	{
+		record_event(ToPhase, _category, _name, &_id);
+	}
+
+private:
+	EventScope(EventScope const&);
+	EventScope& operator = (EventScope const&);
+	char const* _category;
+	char const* _name;
+	uint64_t _id;
+};
+
+typedef EventScope<Phase::BEGIN_EVENT, Phase::END_EVENT> ScopedEvent;
+typedef EventScope<Phase::ASYNC_START, Phase::ASYNC_FINISH> AsyncEvent;
+}
