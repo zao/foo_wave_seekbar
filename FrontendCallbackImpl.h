30ec41770495e70607c0055d3c2a17a683026c0b
Lars Viklund <zao@zao.se>
2014-02-08 22:15:18 +0100

    Mitigate state corruption

--- FrontendCallbackImpl.h
+++ FrontendCallbackImpl.h
@@ -10,7 +10,7 @@ namespace wave
 	struct frontend_callback_impl : visual_frontend_callback, visual_frontend_callback_setter
 	{
 		// Getters
-		virtual double get_track_length() const { return max(DBL_MIN, track_length); }
+		virtual double get_track_length() const { return max(0.001, track_length); }
 		virtual double get_playback_position() const { return playback_position; }
 		virtual bool is_cursor_visible() const { return cursor_visible; }
 		virtual bool is_seeking() const { return seeking; }
